2158b7624d08d4d8b4d4e4610bc36980653239b9
Jakubem <jakubmx@gmail.com>
2017-12-17 22:30:44 +0100

    initial commit

---
+++ convert.py
@@ -0,0 +1,33 @@
+from PIL import Image
+import rawpy
+import numpy
+import imageio
+import glob, os
+
+paths = glob.glob("*.RW2")
+for index, item in enumerate(paths):
+  raw = rawpy.imread(paths[index])
+  rgb = raw.postprocess()
+  imageio.imsave(item + ".tiff", rgb)
+
+dirpath = os.getcwd()
+os.mkdir(dirpath + "/th")
+
+size = 1920, 1080
+  
+for infile in glob.glob("*.tiff"):
+  file, ext = os.path.splitext(infile)
+  im = Image.open(infile)
+  im.thumbnail(size)
+  im.save(dirpath + "/th/" + file + ".jpg")
+
+# for index, item in enumerate(paths):
+#   os.rename(item, str(index) + ".RW2")
+  
+# thumb = glob.glob("th/" + "*.jpg")
+# for index, item in enumerate(thumb):
+#   os.rename(item, "th/" + str(index) + ".jpg")
+
+tiffs = glob.glob("*.tiff")
+for index, item in enumerate(tiffs):
+  os.remove(tiffs[index])
