7d6181723a5e02c28890509840df10850ae5cddc
Jakubem <jakubmx@gmail.com>
2017-12-24 00:44:50 +0100

    different cropping algorithm

--- crop.py
+++ crop.py
@@ -5,20 +5,44 @@ import imageio
 import glob, os
 
 dirpath = os.getcwd()
-os.mkdir(dirpath + "/cropped")
+
+# katalog z kwadratami
+cropped = dirpath + "/cropped"
+
+if not os.path.exists(cropped):
+  os.mkdir(dirpath + "/cropped")
 
 src = sorted(glob.glob("*.jpg"))
 
 for infile in src:
   file, ext = os.path.splitext(infile)
   im = Image.open(infile)
-  short_side = min(im.size)
-  x = (short_side - im.size[0])/2
-  y = (short_side - im.size[1])/2
+  if im.size[0] < im.size[1]:
+    x1 = 0
+    y1 = im.size[1]/2 - im.size[0]/2
+
+    x2 = im.size[0]
+    y2 = im.size[1]/2 + im.size[0]/2
+
+  elif im.size[0] > im.size[1]:
+    
+    x1 = im.size[0]/2 - im.size[1]/2
+    y1 = 0
+
+    x2 = im.size[0]/2 + im.size[1]/2
+    y2 = im.size[1]
+  else:
+    x1 = 0
+    y1 = 0
+    x2 = im.size[0]
+    y2 = im.size[1]
+        
+  # print(im.size)
+  # print(x, y)
   cropped = im.crop((
-    -x,
-    -y,
-    im.size[0] + x,
-    im.size[1] + y
+    x1,
+    y1,
+    x2,
+    y2
   ))
   cropped.save("cropped/" + file + ".jpg")
